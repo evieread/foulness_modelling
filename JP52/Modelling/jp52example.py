c31fad1c88bb52f1f8fbe6364436c480a2268c77
evieread <134697495+evieread@users.noreply.github.com>
2023-06-02 13:03:53 +0100

    Update jp52example.py
    Now loops over 4 parameters not just 2

--- JP52/Modelling/jp52example.py
+++ JP52/Modelling/jp52example.py
@@ -79,14 +79,14 @@ if __name__=='__main__':
 
     # picdir - the directory into which the plots are saved. Will
     # need to be updated for whichever system the user is on.
-    picdir = '/Users/pn22327/Documents/foulnessdata/code/JP52_modelling/temp_pics_2param'
+    picdir = '/Users/pn22327/Documents/foulnessdata/code/JP52_modelling/temp_pics_4param'
 
     # Initialising a dataframe into which I'll put a few parameters
     # of interest:
     # gvecMin -> the minimum group velocity
     # gvecMinFreq -> the frequency at which the minimum group velocity occurs
     # branch3Freq -> the median frequency of the calculated branch 3
-    df = pd.DataFrame(columns=['gvecMin','gvecMinFreq','branch3Freq', 'H', 'beta1'])
+    df = pd.DataFrame(columns=['gvecMin','gvecMinFreq','branch3Freq', 'H', 'beta1', 'alpha1', 'beta2'])
 
     # Here I'm going to set up a base model, and then loop over
     # a number of changes in the shear wave velocity in the
@@ -95,74 +95,68 @@ if __name__=='__main__':
     # Base Model
     alpha0 = 340. # Speed of sound in air (m/s)
     rho0 = 1.29 # Density of air (kg/m^3)
-    alpha1 = 1600. # P-wave speed in upper solid layer (m/s)
+    alpha1 = 1500. # P-wave speed in upper solid layer (m/s)
     rho1 = 1800. # Density of upper solid layer (kg/m^3)
     beta2 = 1000. # Shear-wave speed in lower solid layer (m/s)
     alpha2 = 2700. # P-wave speed in lower solid layer (m/s)
     rho2 = 2000. # Density of lower solid layer (kg/m^3)
-    H = 105. # Thickness of upper solid layer (m)
+    H = 85. # Thickness of upper solid layer (m)
 
 
     # Loop over a set of representative shear-wave speeds for the upper
     # solid layer - note that these are below the acoustic velocity in
     # air.
 
-    Hvec = arange(70., 100., 5.)
+    beta2vec = arange(1000., 1600., 100.)
 
-    beta1vec = arange(220.,290.,5.)
+    alpha1vec = arange(1600., 2000., 100.)
 
-    for Hval in Hvec:
+    Hvec = arange(70., 120., 10.)
 
-        for beta1 in beta1vec:
-            paramdict = {'alpha0':alpha0,
-                        'rho0': rho0,
-                        'beta1':beta1,
-                        'alpha1':alpha1,
-                        'rho1':rho1,
-                        'beta2':beta2,
-                        'alpha2':alpha2,
-                        'rho2':rho2,
-                        'H':Hval}
+    beta1vec = arange(230.,310., 10.)
 
-            # Sets up the model with the parameter dictionary
-            jp52test = jp52base(inputdict=paramdict)
-            # Returns the phase velocity curves (will be plotted if figfile is not None)
-            jp52test.return_phase_vel_curves(pltflag=False,asymptote=0.99) #,figfile=os.path.join(picdir,'test_model_b1_{}_Cvel.png'.format(beta1)))
-            # Returns the group velocity curves (non-dimensionalised)
-            # If pltflag = True, plots the curves to screen
-            jp52test.return_group_vel_curves(pltflag=False)
-            # Now need function to return a frequency vector and a group velocity vector in
-            # 'real' dimensions -> will require a layer thickness (H)
-            # If pltflag = True, plots the curves to screen
-            jp52test.gval_into_physical_units(pltflag=False)
-            # Generate an overview plot
-            jp52test.overview_plot(figfile=os.path.join(picdir,'test_model_b1_{0}_H_{1}.png'.format(beta1, Hval)))
-            plt.show()
+    for beta2val in beta2vec:
 
-            # Now construct a dataframe with some useful properties...
-            dfx = pull_out_group_vel_params(jp52test) # Function at the top of this script.
-            dfx['beta1']= beta1
-            dfx['H']=Hval
-            #print(dfx)
-            df = pd.concat([df,dfx],ignore_index=True)
-            del dfx
+        for alpha1val in alpha1vec:
+
+            for Hval in Hvec:
+
+                for beta1 in beta1vec:
+                    paramdict = {'alpha0':alpha0,
+                                'rho0': rho0,
+                                'beta1':beta1,
+                                'alpha1':alpha1val,
+                                'rho1':rho1,
+                                'beta2':beta2val,
+                                'alpha2':alpha2,
+                                'rho2':rho2,
+                                'H':Hval}
+
+                    # Sets up the model with the parameter dictionary
+                    jp52test = jp52base(inputdict=paramdict)
+                    # Returns the phase velocity curves (will be plotted if figfile is not None)
+                    jp52test.return_phase_vel_curves(pltflag=False,asymptote=0.99) #,figfile=os.path.join(picdir,'test_model_b1_{}_Cvel.png'.format(beta1)))
+                    # Returns the group velocity curves (non-dimensionalised)
+                    # If pltflag = True, plots the curves to screen
+                    jp52test.return_group_vel_curves(pltflag=False)
+                    # Now need function to return a frequency vector and a group velocity vector in
+                    # 'real' dimensions -> will require a layer thickness (H)
+                    # If pltflag = True, plots the curves to screen
+                    jp52test.gval_into_physical_units(pltflag=False)
+                    # Generate an overview plot
+                    jp52test.overview_plot(figfile=os.path.join(picdir,'test_model_b1_{0}_H_{1}_a1_{2}_b2_{3}.png'.format(beta1, Hval, alpha1val, beta2val)))
+                    plt.show()
+
+                    # Now construct a dataframe with some useful properties...
+                    dfx = pull_out_group_vel_params(jp52test) # Function at the top of this script.
+                    dfx['beta1']= beta1
+                    dfx['H']=Hval
+                    dfx['alpha1']= alpha1val
+                    dfx['beta2']= beta2val
+                    #print(dfx)
+                    df = pd.concat([df,dfx],ignore_index=True)
+                    del dfx
 
     print(df)
-    df.to_csv('tempdf.csv')
-    # - Now an example test figure showing results....
-    fig = plt.figure(figsize=(7.0,5.0))
-    ax1 = fig.add_axes([0.15, 0.15, 0.60, 0.80])
-    # ax2 = ax1.twinx()
-    # ax1.plot(df['beta1'],df['gvecMin'],'ko')
-    # ax2.plot(df['H'],df['gvecMin'],'ro',mfc='none')
-    # ax1.set_xlabel(r'$\beta_{1}$ (m/s')
-    # ax1.set_ylabel('Group Velocity Minimum (m/s)')
-    # ax2.set_ylabel('Frequency of Group\nVelocity Minimum (Hz)',color='r')
-    # ax2.tick_params(axis='y', labelcolor='r')
-    ax1.scatter(df['beta1'],df['H'], c=df['gvecMin'])
-    plt.show()
-
-    # A figure showing the frequency of the airwave could also be done - I'll leave that to you!
-    # The median frequency value of the Branch III estimates can be found in the dataframe within
-    # the column df['branch3Freq']
+    df.to_csv('/Users/pn22327/Documents/foulnessdata/code/JP52_modelling/JP52_tempdf.csv')
     
